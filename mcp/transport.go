e6cf5be96d61b6b082edfdc96878098805571918
Rishabh Nrupnarayan <aryannr97@gmail.com>
2025-07-31 18:28:07 +0530

    refactor: inline condition check

--- mcp/transport.go
+++ mcp/transport.go
@@ -423,15 +423,12 @@ func (t *ioConn) Read(ctx context.Context) (jsonrpc.Message, error) {
 
 	// Read the next byte to check if there is trailing data.
 	var tr [1]byte
-	n, err := in.Buffered().Read(tr[:])
-	if n > 0 {
+	if n, err := in.Buffered().Read(tr[:]); n > 0 {
 		// If read byte is not a newline, it is an error.
 		if tr[0] != '\n' {
 			return nil, fmt.Errorf("invalid trailing data at the end of stream")
 		}
-	}
-	// Return error except for EOF
-	if err != nil && err != io.EOF {
+	} else if err != nil && err != io.EOF {
 		return nil, err
 	}
 
