46aeccf4aa0f7ccdd9c63acd31378b694b25c9ca
FanOne <294350394@qq.com>
2026-02-02 00:07:48 +0800

    feat: rename unit test

---
+++ mcp/example_session_error_test.go
@@ -0,0 +1,14 @@
+// Copyright 2025 The Go MCP SDK Authors. All rights reserved.
+// Use of this source code is governed by an MIT-style
+// license that can be found in the LICENSE file.
+
+package mcp_test
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"net/http"
+	"net/http/httptest"
+
+)
